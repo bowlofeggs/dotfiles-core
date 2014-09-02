b4ed2c65fdc16d3d72e63b66f09643f02d3d18e6
robled <robled@gmail.com>
2014-09-02 09:47:46 -0400

    initial commit

---
+++ bin/custom-bashrc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
+
+if grep 'tmux' ~/.bashrc
+then
+    echo 'tmux already configured'
+    exit 0
+else
+cat <<EOF >> ~/.bashrc
+
+# tmux!
+if [ -z "$SSH_TTY" ]; then
+    # If not running interactively, do not do anything
+    [[ $- != *i* ]] && return
+    [[ -z "$TMUX" ]] && exec tmux
+else
+    if which tmux >/dev/null 2>&1; then
+        #if not inside a tmux session, and if no session is started, start a new session
+        test -z "$TMUX" && (tmux attach || tmux new-session)
+    fi
+fi
+EOF
+fi
